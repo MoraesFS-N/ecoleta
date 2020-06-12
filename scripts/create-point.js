f0e943a9db4490b3de488e4cf9eced6daeb52bf7
Moraes Neto <moraesfelicioneto@hotmail.com>
2020-06-12 11:03:01 -0300

    Segunda parte do cadastro de ponto de coleta.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -25,16 +25,44 @@ function getCities(event) {
     stateInput.value = event.target.options[indexOfSelectedState].text;
 
     const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios';
+    citySelect.innerHTM = '<option value> Selecione a cidade></option>';
+    citySelect.disabled = true;
 
     fetch(url)
     .then( res => res.json())
     .then( cities => {
-
         for (const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
         }
     citySelect.disabled= false;
     })
 }
 
 document.querySelector('select[name=uf]').addEventListener('change', getCities);
+
+// itens de coleta
+const itensToCollect = document.querySelectorAll('.itens-grid');
+
+for (const item of itensToCollect) {
+    item.addEventListener('click', handleSelectedItem);
+}
+
+let selectedItens = [];
+
+
+function handleSelectedItem(event) {
+    const itemLi = event.target;
+    // adicionar ou remover um classe
+    itemLi.classList.toggle('selected');
+
+    const itemId = event.target.dataset.id;
+
+    // verificar se existem itens selecionados, se sim, pegar os itens selecionados 
+    const alreadySelected = selectedItens.findIndex( (item)=>{
+       const itemFound = item === itemId
+    })
+
+    //se já estiver selecionadom tirar da seleção 
+
+    // se não tiver selecionado, adicionar a seleção
+}
