c1b00c11564864856abbaba9e9a728c022d4d4d6
Moraes Neto <moraesfelicioneto@hotmail.com>
2020-06-20 10:55:23 -0300

    Criação do banco de dados

---
+++ src/database/db.js
@@ -0,0 +1,77 @@
+// configurando o sqlite para visualizar mensagens no terminal com a função verbose 
+const sqlite3 = require('sqlite3').verbose();
+
+// criar objeto que irá fazer operações no banco de dados
+const db = new sqlite3.Database('./src/database/database.db');
+
+// utilizar o objeto de bd para executar suas funções
+db.serialize(() => {
+    //Criar uma tabela com comandos sql
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            image TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `)
+    
+    const query = `
+        INSERT INTO places (
+            name,
+            image,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?, ?, ?, ?, ?, ?, ?);`
+
+    const values = ['Colectoria',
+                    'https://images.unsplash.com/photo-1516992654410-9309d4587e94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+                    'Guilherme Gemballa, Jardim América',
+                    'Nº 465',
+                    'Santa Catarina',
+                    'Rio do Sul',
+                    'Resíduos eletrônicos'
+    ]
+
+
+    function afterInsertData(err){
+        if(err){ 
+            return console.log(err);
+        }
+    
+        console.log('Cadastrado com sucesso');
+        console.log(this);
+    }
+
+    // db.run(query, values, afterInsertData);
+    // o rows mostra todos os dados inseridos na tabela em linha
+    db.all(`SELECT * FROM places`, function(err, rows) {
+        if(err){ 
+            return console.log(err);
+        }
+        console.log('Aqui estão seus registros');
+        console.log(rows);
+    })
+
+    db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
+        if(err){ 
+            return console.log(err);
+        }
+        console.log('Registro deletado com sucesso');
+    });
+    
+    // db.all(`SELECT * FROM places`, function(err, rows) {
+    //     if(err){ 
+    //         return console.log(err);
+    //     }
+    //     console.log('Aqui estão seus registros');
+    //     console.log(rows);
+    // })
+})
