0a600fa425edf7b07fc5480300b43a419e1f0319
Moraes Neto <moraesfelicioneto@hotmail.com>
2020-06-20 10:56:02 -0300

    atualização banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -60,18 +60,18 @@ db.serialize(() => {
         console.log(rows);
     })
 
-    db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
-        if(err){ 
-            return console.log(err);
-        }
-        console.log('Registro deletado com sucesso');
-    });
-    
-    // db.all(`SELECT * FROM places`, function(err, rows) {
+    // db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
     //     if(err){ 
     //         return console.log(err);
     //     }
-    //     console.log('Aqui estão seus registros');
-    //     console.log(rows);
-    // })
+    //     console.log('Registro deletado com sucesso');
+    // });
+    
+    db.all(`SELECT * FROM places`, function(err, rows) {
+        if(err){ 
+            return console.log(err);
+        }
+        console.log('Aqui estão seus registros');
+        console.log(rows);
+    })
 })
